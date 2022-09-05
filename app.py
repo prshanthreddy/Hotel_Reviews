8f98d823e8a1f65b7206c863d2b241f75442ce81
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2022-09-05 13:56:39 +0530

    This commit creates webapp

---
+++ app.py
@@ -0,0 +1,26 @@
+import imp
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import pandas as pd
+import numpy as np
+
+app=Flask(__name__)
+# load the model
+mlmodel=pickle.load(open('mlmodel.pickle','rb'))
+vectorizer=pickle.load(open('vectorizer.pickle','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    if request.method=="POST":
+        data=request.form['message']
+        data=[data]
+        new_data=vectorizer.transform(data).toarray()
+        output=mlmodel.predict(new_data)
+    return render_template('home.html',prediction=output)
+    
+if __name__=="__main":
+    app.run(debug=True)
