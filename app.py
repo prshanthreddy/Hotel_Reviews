bde650a3cb17be42586dc4bfdceee1f11ca3f295
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2023-02-08 11:06:45 +0530

    Update app.py
    Streamlit app

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
+import streamlit as st 
 import pickle
-from flask import Flask,request,render_template,url_for
 import pandas as pd
-
-app=Flask(__name__)
-# load the model
+import numpy as np
+st.title("Welcome to Hotel Review Prediction App")
+st.header("Enter your review below")
+review = st.text_area("Review")
 mlmodel=pickle.load(open('mlmodel.pickle','rb'))
 vectorizer=pickle.load(open('vectorizer.pickle','rb'))
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    if request.method=="POST":
-        data=request.form['message']
-        data=[data]
+data=pd.read_csv("Exaggeration in fake vs. authentic online reviews for luxury and budget hotels .csv")
+if st.button("Predict"):
+    if review == "":
+        st.warning("Please enter a review")
+    else:
+        data=[review]
         new_data=vectorizer.transform(data).toarray()
         output=mlmodel.predict(new_data)
-    return render_template('home.html',prediction=output)
-    
-if __name__=="__main__":
-    app.run(debug=True)
+        if output[0]=='truth':
+            st.success("This review is authentic")
+        else:
+            st.error("This review is fake")
+st.warning("The accuracy of the model is 0.97")
+st.write("This app is made by Prashanth Reddy")
+st.write("You can connect me on LinkedIn:  https://www.linkedin.com/in/prashanthreddy07/")
